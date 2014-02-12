43e967965eed43baaa9e70c7a04f06c9400a6531
Nicolas Petton <petton.nicolas@gmail.com>
2014-02-12 14:34:38 +0100

    rename to Tide

---
+++ Tide-Amber/js/Tide-Amber-Examples.js
@@ -0,0 +1,217 @@
+define("timetravel/Tide-Amber-Examples", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_vm/globals", "amber_core/Canvas"], function(smalltalk,nil,_st, globals){
+smalltalk.addPackage('Tide-Amber-Examples');
+smalltalk.packages["Tide-Amber-Examples"].transport = {"type":"amd","amdNamespace":"timetravel"};
+
+smalltalk.addClass('TTCounterWidget', globals.Widget, ['counter', 'header'], 'Tide-Amber-Examples');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "addNewCounter",
+protocol: 'actions',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+_st(_st(self._counter())._otherCounter())._then_((function(proxy){
+return smalltalk.withContext(function($ctx2) {
+return _st(_st(self._class())._on_(proxy))._render();
+}, function($ctx2) {$ctx2.fillBlock({proxy:proxy},$ctx1,1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"addNewCounter",{},smalltalk.TTCounterWidget)})},
+args: [],
+source: "addNewCounter\x0a\x09self counter otherCounter then: [ :proxy | \x0a\x09\x09(self class on: proxy) render ]",
+messageSends: ["then:", "otherCounter", "counter", "render", "on:", "class"],
+referencedClasses: []
+}),
+globals.TTCounterWidget);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "counter",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+function $TTProxy(){return smalltalk.TTProxy||(typeof TTProxy=="undefined"?nil:TTProxy)}
+return smalltalk.withContext(function($ctx1) { 
+var $2,$1;
+$2=self["@counter"];
+if(($receiver = $2) == nil || $receiver == null){
+self["@counter"]=_st($TTProxy())._on_("/counter");
+$1=self["@counter"];
+} else {
+$1=$2;
+};
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"counter",{},smalltalk.TTCounterWidget)})},
+args: [],
+source: "counter\x0a\x09^ counter ifNil: [ counter := TTProxy on: '/counter' ]",
+messageSends: ["ifNil:", "on:"],
+referencedClasses: ["TTProxy"]
+}),
+globals.TTCounterWidget);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "counter:",
+protocol: 'accessing',
+fn: function (aCounter){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+self["@counter"]=aCounter;
+return self}, function($ctx1) {$ctx1.fill(self,"counter:",{aCounter:aCounter},smalltalk.TTCounterWidget)})},
+args: ["aCounter"],
+source: "counter: aCounter\x0a\x09counter := aCounter",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTCounterWidget);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "decrease",
+protocol: 'actions',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+_st(self._counter())._decrease();
+_st(self._counter())._then_((function(){
+return smalltalk.withContext(function($ctx2) {
+return self._update();
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1,1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"decrease",{},smalltalk.TTCounterWidget)})},
+args: [],
+source: "decrease\x0a\x09self counter decrease.\x0a\x09self counter then: [ self update ]",
+messageSends: ["decrease", "counter", "then:", "update"],
+referencedClasses: []
+}),
+globals.TTCounterWidget);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "increase",
+protocol: 'actions',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+_st(self._counter())._increase();
+_st(self._counter())._then_((function(){
+return smalltalk.withContext(function($ctx2) {
+return self._update();
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1,1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"increase",{},smalltalk.TTCounterWidget)})},
+args: [],
+source: "increase\x0a\x09self counter increase.\x0a\x09self counter then: [ self update ]",
+messageSends: ["increase", "counter", "then:", "update"],
+referencedClasses: []
+}),
+globals.TTCounterWidget);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "render",
+protocol: 'rendering',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+_st(self._counter())._connect();
+_st(self._counter())._then_((function(){
+return smalltalk.withContext(function($ctx2) {
+return self._appendToJQuery_("body"._asJQuery());
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1,1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"render",{},smalltalk.TTCounterWidget)})},
+args: [],
+source: "render\x0a\x09self counter connect.\x0a\x09self counter then: [ \x0a\x09\x09self appendToJQuery: 'body' asJQuery ]",
+messageSends: ["connect", "counter", "then:", "appendToJQuery:", "asJQuery"],
+referencedClasses: []
+}),
+globals.TTCounterWidget);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "renderOn:",
+protocol: 'rendering',
+fn: function (html){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $1,$2,$3,$4,$5,$6;
+self["@header"]=_st(_st(html)._h1())._with_(_st(_st(self._counter())._count())._asString());
+$1=_st(html)._button();
+_st($1)._with_("++");
+$2=_st($1)._onClick_((function(){
+return smalltalk.withContext(function($ctx2) {
+return self._increase();
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1,1)})}));
+$3=_st(html)._button();
+_st($3)._with_("--");
+$4=_st($3)._onClick_((function(){
+return smalltalk.withContext(function($ctx2) {
+return self._decrease();
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1,2)})}));
+_st(html)._br();
+$5=_st(html)._a();
+_st($5)._with_("add a new counter");
+$6=_st($5)._onClick_((function(){
+return smalltalk.withContext(function($ctx2) {
+return self._addNewCounter();
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1,3)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"renderOn:",{html:html},smalltalk.TTCounterWidget)})},
+args: ["html"],
+source: "renderOn: html\x0a\x09header := html h1 with: self counter count asString.\x0a\x09html button \x0a\x09\x09with: '++';\x0a\x09\x09onClick: [ self increase ].\x0a\x09html button \x0a\x09\x09with: '--';\x0a\x09\x09onClick: [ self decrease ].\x0a\x09html br.\x0a\x09html a\x0a\x09\x09with: 'add a new counter';\x0a\x09\x09onClick: [ self addNewCounter ]",
+messageSends: ["with:", "h1", "asString", "count", "counter", "button", "onClick:", "increase", "decrease", "br", "a", "addNewCounter"],
+referencedClasses: []
+}),
+globals.TTCounterWidget);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "update",
+protocol: 'updating',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+_st(self["@header"])._contents_((function(html){
+return smalltalk.withContext(function($ctx2) {
+return _st(html)._with_(_st(_st(self._counter())._count())._asString());
+}, function($ctx2) {$ctx2.fillBlock({html:html},$ctx1,1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"update",{},smalltalk.TTCounterWidget)})},
+args: [],
+source: "update\x0a\x09header contents: [ :html |\x0a\x09\x09html with: self counter count asString ]",
+messageSends: ["contents:", "with:", "asString", "count", "counter"],
+referencedClasses: []
+}),
+globals.TTCounterWidget);
+
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "initialize",
+protocol: 'not yet classified',
+fn: function (){
+var self=this;
+return self},
+args: [],
+source: "initialize\x0a\x09\x22self new render\x22",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTCounterWidget.klass);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "on:",
+protocol: 'not yet classified',
+fn: function (aProxy){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $2,$3,$1;
+$2=self._new();
+_st($2)._counter_(aProxy);
+$3=_st($2)._yourself();
+$1=$3;
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"on:",{aProxy:aProxy},smalltalk.TTCounterWidget.klass)})},
+args: ["aProxy"],
+source: "on: aProxy\x0a\x09^ self new\x0a\x09\x09counter: aProxy;\x0a\x09\x09yourself",
+messageSends: ["counter:", "new", "yourself"],
+referencedClasses: []
+}),
+globals.TTCounterWidget.klass);
+
+});
---
+++ Tide-Amber/js/Tide-Amber-Exceptions.js
@@ -0,0 +1,148 @@
+define("timetravel/Tide-Amber-Exceptions", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_vm/globals", "amber_core/Kernel-Exceptions"], function(smalltalk,nil,_st, globals){
+smalltalk.addPackage('Tide-Amber-Exceptions');
+smalltalk.packages["Tide-Amber-Exceptions"].transport = {"type":"amd","amdNamespace":"timetravel"};
+
+smalltalk.addClass('TTPromiseNotFound', globals.Error, ['selector'], 'Tide-Amber-Exceptions');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "messageText",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $1;
+$1="No promise was found for selector ".__comma(self._selector());
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"messageText",{},globals.TTPromiseNotFound)})},
+args: [],
+source: "messageText\x0a\x09^ 'No promise was found for selector ', self selector",
+messageSends: [",", "selector"],
+referencedClasses: []
+}),
+globals.TTPromiseNotFound);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "selector",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+var $1;
+$1=self["@selector"];
+return $1;
+},
+args: [],
+source: "selector\x0a\x09^ selector",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTPromiseNotFound);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "selector:",
+protocol: 'accessing',
+fn: function (aString){
+var self=this;
+self["@selector"]=aString;
+return self},
+args: ["aString"],
+source: "selector: aString\x0a\x09selector := aString",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTPromiseNotFound);
+
+
+
+smalltalk.addClass('TTRequestError', globals.Error, ['xhr'], 'Tide-Amber-Exceptions');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "xhr",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $1;
+$1=self["@xhr"];
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"xhr",{},smalltalk.TTRequestError)})},
+args: [],
+source: "xhr\x0a\x09^ xhr",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTRequestError);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "xhr:",
+protocol: 'accessing',
+fn: function (anObject){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+self["@xhr"]=anObject;
+return self}, function($ctx1) {$ctx1.fill(self,"xhr:",{anObject:anObject},smalltalk.TTRequestError)})},
+args: ["anObject"],
+source: "xhr: anObject\x0a\x09xhr := anObject",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTRequestError);
+
+
+
+smalltalk.addClass('TTServerError', globals.Error, ['code'], 'Tide-Amber-Exceptions');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "code",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+var $1;
+$1=self["@code"];
+return $1;
+},
+args: [],
+source: "code\x0a\x09^ code",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTServerError);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "code:",
+protocol: 'accessing',
+fn: function (aString){
+var self=this;
+self["@code"]=aString;
+return self},
+args: ["aString"],
+source: "code: aString \x0a\x09code := aString.",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTServerError);
+
+
+
+smalltalk.addClass('TTSessionNotFound', globals.Error, [], 'Tide-Amber-Exceptions');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "messageText",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+return "Tide session not found";
+}, function($ctx1) {$ctx1.fill(self,"messageText",{},smalltalk.TTSessionNotFound)})},
+args: [],
+source: "messageText\x0a\x09^ 'Tide session not found'",
+messageSends: [],
+referencedClasses: []
+}),
+globals.TTSessionNotFound);
+
+
+});
